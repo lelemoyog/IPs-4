b88be7b08bd73be9323cf52ce5451c07774a0924
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-06-26 14:28:27 +0300

    added toggle to add to cart button to toggle append box

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+function Order(crust ,flavour ,topping ,size){
+    this.crust = crust;
+    this.flavour= flavour;
+    this.topping= topping;
+    this.size= size;
+}
+Order.prototype.newOrder= function(){
+    return "<li>"+this.crust + " </br>"+"<li>"+this.flavour+ "</br> "+"<li>"+this.topping + " </br>"+"<li>"+this.size +"</li>"
+}
+function PriceToppings(mozzarella,fetaCheese ,pineapple){
+    this.mozzarella=250;
+    this.fetaCheese=150;
+    this.pineapple=50;
+}
+
+
+
+
 $(document).ready(function(){
    
     $('form#order').hide (); 
@@ -8,22 +26,27 @@ $(document).ready(function(){
 
         $('form#order').toggle()
     });
-    $("button#bttn").click(function(event){
+    $("button#bttn,button#bttnn").click(function(event){
         event.preventDefault();
         $("div#appnd").toggle()
 
-         var crust =$("input[name='group2']:checked").val();
-         var flavour =$("input[name='group1']:checked").val();
-         var topping =$("input[name='group3']:checked").val();
-         var size =$("input[name='group4']:checked").val();
+         var inputcrust =$("input[name='group2']:checked").val();
+         var inputflavour =$("input[name='group1']:checked").val();
+         var inputtopping =$("input[name='group3']:checked").val();
+         var inputsize =$("input[name='group4']:checked").val();
+          
+         var neworder = new Order(inputcrust,inputflavour,inputtopping,inputsize )
+        $("#appededList").append(neworder.newOrder());
+        console.log(neworder)
 
 
 
 
-         console.log(crust);
-         console.log(flavour);
-          console.log(topping);
-         console.log(size);
+
+        //  console.log(crust);
+        //  console.log(flavour);
+        //   console.log(topping);
+        //  console.log(size);
 
 
            })
