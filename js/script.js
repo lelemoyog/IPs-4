430e475312a89fe7593266645351c822b554be83
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-06-26 12:03:58 +0300

    added form data collection

--- js/script.js
+++ js/script.js
@@ -11,9 +11,26 @@ $(document).ready(function(){
     $("button#bttn").click(function(event){
         event.preventDefault();
         $("div#appnd").toggle()
-    })
+
+         var crust =$("input[name='group2']:checked").val();
+         var flavour =$("input[name='group1']:checked").val();
+         var topping =$("input[name='group3']:checked").val();
+         var size =$("input[name='group4']:checked").val();
+
+
+
+
+         console.log(crust);
+         console.log(flavour);
+          console.log(topping);
+         console.log(size);
+
+
+           })
 
     
+   
+
 });
 
 
